f073aebfc8e5b97333c8de6754f94e095d14d64d
Leiting Li <lileiting@gmail.com>
2015-05-28 08:28:12 +0800

    Revert "Revert "Revert "Revert "Try to read directly from pileup file, rather than pileup matrix""""
    This reverts commit 5ab00589db385300c094de79cef8c10b049e2c69.

--- pileuptosnp.pl
+++ pileuptosnp.pl
@@ -11,7 +11,22 @@ use Getopt::Long;
 sub usage{
     print <<USAGE;
 
-perl pileuptosnp.pl <genome.pileup.matrix.txt> [OPTIONS]
+perl pileuptosnp.pl [OPTIONS]
+
+  -m,--male <male.pileup.txt>
+  -f,--female <female.pileup.txt>
+  -p,--progeny <progeny1.pileup.txt>[,<progeny2.pielup.txt>...]
+  [-p,--progeny <progeny3.pileup.txt> ...]
+    The pileup file must be sorted, or the program will stop
+
+  -i,--index <genome.index.txt>
+    The index file includes the first two columns of the pileup 
+    format from all pileup files indicated by -m, -f and -p.
+    Commands to get the index file is like this:
+    cat *.pielup.txt | grep -v '^\[' | cut -f 1,2 | 
+        sort | uniq > genome.index.txt
+    If the index file do not exist, the program will create one
+    using the commands above.
 
   -t,--threshold NUM
     threshold
@@ -23,19 +38,62 @@ USAGE
 }
 
 ############################################################
+# Main
+############################################################
+
+sub main{
+    my $para = read_commands;
+    my $index = load_index_file($para);
+    process_pileup($para);
+    exit 0;
+}
+
+############################################################
 # Read commands
 ############################################################
 
 sub read_commands{
-    my %para = (threshold => 3);
+    my %para = (
+        male => '',
+        female => '',
+        threshold => 3
+    );
 
-    GetOptions ("help" => \$para{help},
-                "threshold" => \$para{threshold}
-               );
+    GetOptions(
+        "m|male=s" => \$para{male},
+        "f|female=s" => \$para{female},
+        "p|progeny" => \@{$para{progeny}},
+        "i|index" => \$para{index},
+        "t|threshold" => \$para{threshold},
+        "h|help" => \$para{help}
+    );
+    @{$para{progeny}} = split(/,/,join(',',@{$para{progeny}}));
+    
+    die "No parents!" unless ($para{male} or $para{female});
+    die "No progenies!" unless @{$para{progeny}};
 
     usage if $para{help} or @ARGV == 0;
-    $para{infile} = shift @ARGV;
+
     return \%para;
+}
+
+###########################################################
+# Load index file
+###########################################################
+
+sub load_index_file{
+    my $para = shift;
+    my $index_file = $para->{index};
+    unless(-e $index_file){
+        my @files = ($para->{male}, $para->{female}, @{$para->{progeny}});
+        system "cat @files | cut -f 1,2 | sort | uniq > $index_file";
+    }
+
+    open my $fh, "<", $index_file or die $!;
+    while(<$fh>){
+
+    }
+    close $fh;
 }
 
 ############################################################
@@ -140,15 +198,6 @@ sub process_pileup{
         print "\n";
     }
     close $in_fh;
-}
-
-############################################################
-# Main
-############################################################
-
-sub main{
-    my $para = read_commands;
-    process_pileup($para);
 }
 
 main();
